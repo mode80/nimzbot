be6d5853346339b13a894196718483bdfb5a13c6
mode80 <mode80@users.noreply.github.com>
2023-01-23 20:26:56 -0700

    used_upper_slots

--- yahtzbot.nim
+++ yahtzbot.nim
@@ -1,7 +1,7 @@
 
 {. hint[XDeclaredButNotUsed]:off .}
 # {.experimental: "codeReordering".}
-import macros, options, tables, sequtils, math, algorithm
+import macros, options, tables, sequtils, math, algorithm, bitops
 # -------------------------------------------------------------
 # TYPES 
 
@@ -103,6 +103,11 @@ func unique_permutations (sorted_list :seq[int]) :seq[seq[int]] =
     while list.nextPermutation:
         if not result.contains(list): result.add(list)
 
+func inverse [T] (s :set[T]) :set[T] =
+    ## returns the inverse of a set, i.e. the set of all elements not in the input set
+    for i in T.low..T.high: 
+        if not s.contains(i): result.incl(i)
+
 # -------------------------------------------------------------
 # DIEVALS 
 # -------------------------------------------------------------
@@ -182,25 +187,23 @@ type Slots = set[SlotType]
 
 using self: Slots
 
+
 func init_slots(args: varargs[int]): Slots = ## construct a Slots from a varargs of Slot args) 
     for arg in args: result.incl arg.SlotType
+
 
 func `$`(self) :string = ## convert a Slots to a string
     for slot in self:
         result.add $slot.int
         result.add '_' 
+
+
+func used_upper_slots(unused_slots :Slots) :Slots =
+    const upper_slots = {ACES, TWOS, THREES, FOURS, FIVES, SIXES}
+    var used_slots = unused_slots.inverse 
+    result = used_slots * upper_slots # intersection 
+
 #[
-
-int* ZERO_THRU_63 = (int[64]){0,1,2,3,4,5,6,7,8,9, 10,11,12,13,14,15,16,17,18,19,
-        20,21,22,23,24,25,26,27,28,29, 30,31,32,33,34,35,36,37,38,39,
-        40,41,42,43,44,45,46,47,48,49, 50,51,52,53,54,55,56,57,58,59, 60,61,62,63};
-
-Slots used_upper_slots(Slots unused_slots) {
-    Slots all_bits_except_unused = ~unused_slots; // "unused" slots are not "previously used", so blank those out
-    Slots all_upper_slot_bits = (u16) ((1<<7)-2);  // upper slot bits are those from 2^1 through 2^6 (encoding doesn't use 2^0)
-    Slots previously_used_upper_slot_bits = (u16) (all_bits_except_unused & all_upper_slot_bits);
-    return previously_used_upper_slot_bits;
-} 
 
 void slots_powerset(Slots self, Slots* out, int* out_len) { 
     int len = slots_count(self);
@@ -506,4 +509,5 @@ when isMainModule:
     echo slots.len
     echo slots.contains(FULL_HOUSE)
     echo slots
+    echo used_upper_slots(unused_slots=slots)
 
